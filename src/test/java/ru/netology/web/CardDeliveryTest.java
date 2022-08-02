2e8fc62a0a1eac24d8d143a13e4c18e0c4a71f69
nrr <nrr@warsong.me>
2022-08-02 08:09:47 +0700

    throw from laptop to PC

---
+++ src/test/java/ru/netology/web/CardDeliveryTest.java
@@ -0,0 +1,50 @@
+package ru.netology.web;
+
+import com.codeborne.selenide.Condition;
+import net.bytebuddy.asm.Advice;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.Keys;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+
+import static com.codeborne.selenide.Condition.exactText;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selenide.$x;
+import static com.codeborne.selenide.Selenide.open;
+
+public class CardDeliveryTest {
+    public final static DateTimeFormatter DATE_FORMATTER = DateTimeFormatter
+            .ofPattern("dd/MM/yyyy");
+//    private LocalDate dt = LocalDate.now().plusDays(3);
+//    private int year = dt.getYear();
+//    private int month = dt.getMonthValue();
+//    private int day = dt.getDayOfMonth();
+    private LocalDate dt = LocalDate.now().plusDays(3);
+    String date = DATE_FORMATTER.format(dt);
+
+    @BeforeEach
+    public void openCardForm() {
+        open("http://localhost:9999");
+    }
+
+    @Test
+    void shouldPassValidation() {
+        $x("//input[@placeholder='Город']").setValue("Краснодар");
+//        $x("//*[@data-test-id='date']").doubleClick();
+//        $x("//*[@data-test-id='date']").sendKeys(Keys.BACK_SPACE);
+//        $x("//*[@data-test-id='date']").setValue(date);
+        $x("//*[@placeholder='Дата встречи']").doubleClick();
+        $x("//*[@placeholder='Дата встречи']").sendKeys(Keys.BACK_SPACE);
+        $x("//*[@placeholder='Дата встречи']").setValue(date);
+        $x("//input[@name='name']").setValue("Дмитрий Мамин-Сибиряк");
+        $x("//input[@name='phone']").setValue("+79012345678");
+        //$x("//input[@name='agreement']").click();
+        $x("//span[@class='checkbox__box']").click();
+        $x("//button[@class='button button_view_extra button_size_m button_theme_alfa-on-white']").click();
+        $x("//*[@data-test-id='notification']").shouldBe(visible, Duration.ofSeconds(15));
+        $x("//*[@data-test-id='notification']").should(Condition.matchText("Успешно!"));
+    }
+}
