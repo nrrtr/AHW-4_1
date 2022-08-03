97d27e68becc9cd38fb57311d63175fff71076d4
nrr <nrr@warsong.me>
2022-08-04 04:13:51 +0700

    reformate code after feedback

--- src/test/java/ru/netology/web/CardDeliveryTest.java
+++ src/test/java/ru/netology/web/CardDeliveryTest.java
@@ -3,26 +3,30 @@ package ru.netology.web;
 import com.codeborne.selenide.Condition;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Random;
 
 import static com.codeborne.selenide.Condition.exactText;
 import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
 
 public class CardDeliveryTest {
-    public final static DateTimeFormatter DATE_FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy");
-    private LocalDate dt = LocalDate.now().plusDays(3);
-    private String date = DATE_FORMATTER.format(dt);
+    private Random random = new Random();
+    private String date = generateDate(3);
+
+    public String generateDate(int days) {
+        return LocalDate.now().plusDays(days).format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
+    }
 
     @BeforeEach
     public void openCardForm() {
         open("http://localhost:9999");
     }
+
 
     @Test
     void shouldPassValidation() {
@@ -33,12 +37,13 @@ public class CardDeliveryTest {
         $x("//input[@name='name']").setValue("Дмитрий Мамин-Сибиряк");
         $x("//input[@name='phone']").setValue("+79012345678");
         $x("//span[@class='checkbox__box']").click();
-        $x("//button[@class='button button_view_extra button_size_m button_theme_alfa-on-white']").click();
+        $(".button [class='button__text']").click();
         $x("//*[@data-test-id='notification']").shouldBe(visible, Duration.ofSeconds(15));
         $x("//*[@data-test-id='notification']").should(Condition.matchText("Успешно!"));
-    }
+        $("[data-test-id='notification'] .notification__content").shouldHave(Condition
+                .text("Встреча успешно забронирована на " + date));
 
-    //not null suite
+    }
 
     @Test
     void shouldShowNotNullMessageWithCityField() {
@@ -48,9 +53,9 @@ public class CardDeliveryTest {
         $x("//input[@name='name']").setValue("Дмитрий Мамин-Сибиряк");
         $x("//input[@name='phone']").setValue("+79012345678");
         $x("//span[@class='checkbox__box']").click();
-        $x("//button[@class='button button_view_extra button_size_m button_theme_alfa-on-white']").click();
-        $$("[class='input__sub']").get(0).shouldBe(visible);
-        $$("[class='input__sub']").get(0).shouldHave(exactText("Поле обязательно для заполнения"));
+        $(".button [class='button__text']").click();
+        $("[data-test-id='city'] .input__sub").shouldBe(visible)
+                .shouldHave(exactText("Поле обязательно для заполнения"));
     }
 
     @Test
@@ -61,9 +66,9 @@ public class CardDeliveryTest {
         $x("//input[@name='name']").setValue("Мамин Дмитрий-Сибиряк");
         $x("//input[@name='phone']").setValue("+75678901234");
         $x("//span[@class='checkbox__box']").click();
-        $x("//button[@class='button button_view_extra button_size_m button_theme_alfa-on-white']").click();
-        $$("[class='input__sub']").get(1).shouldBe(visible);
-        $$("[class='input__sub']").get(1).shouldHave(exactText("Неверно введена дата"));
+        $(".button [class='button__text']").click();
+        $("[data-test-id='date'] .input__sub").shouldBe(visible)
+                .shouldHave(exactText("Неверно введена дата"));
     }
 
     @Test
@@ -74,9 +79,9 @@ public class CardDeliveryTest {
         $x("//*[@placeholder='Дата встречи']").setValue(date);
         $x("//input[@name='phone']").setValue("+77890561234");
         $x("//span[@class='checkbox__box']").click();
-        $x("//button[@class='button button_view_extra button_size_m button_theme_alfa-on-white']").click();
-        $$("[class='input__sub']").get(2).shouldBe(visible);
-        $$("[class='input__sub']").get(2).shouldHave(exactText("Поле обязательно для заполнения"));
+        $(".button [class='button__text']").click();
+        $("[data-test-id='name'] .input__sub").shouldBe(visible)
+                .shouldHave(exactText("Поле обязательно для заполнения"));
     }
 
     @Test
@@ -87,9 +92,9 @@ public class CardDeliveryTest {
         $x("//*[@placeholder='Дата встречи']").setValue(date);
         $x("//input[@name='name']").setValue("Сибиряк Дмитрий-Мамин");
         $x("//span[@class='checkbox__box']").click();
-        $x("//button[@class='button button_view_extra button_size_m button_theme_alfa-on-white']").click();
-        $$("[class='input__sub']").get(3).shouldBe(visible);
-        $$("[class='input__sub']").get(3).shouldHave(exactText("Поле обязательно для заполнения"));
+        $(".button [class='button__text']").click();
+        $("[data-test-id='phone'] .input__sub").shouldBe(visible)
+                .shouldHave(exactText("Поле обязательно для заполнения"));
     }
 
     @Test
@@ -100,13 +105,10 @@ public class CardDeliveryTest {
         $x("//*[@placeholder='Дата встречи']").setValue(date);
         $x("//input[@name='name']").setValue("Сибиряк Дмитрий-Мамин");
         $x("//input[@name='phone']").setValue("+75612347890");
-        $x("//button[@class='button button_view_extra button_size_m button_theme_alfa-on-white']").click();
-        $("[data-test-id='agreement'].input_invalid").shouldBe(visible);
-        $("[data-test-id='agreement'].input_invalid").shouldHave(
-                exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных"));
+        $(".button [class='button__text']").click();
+        $("[data-test-id='agreement'].input_invalid").shouldBe(visible)
+                .shouldHave(exactText("Я соглашаюсь с условиями обработки и использования моих персональных данных"));
     }
-
-    //wrong suite
 
     @Test
     void shouldShowWrongCityMessage() {
@@ -117,9 +119,9 @@ public class CardDeliveryTest {
         $x("//input[@name='name']").setValue("Дмитрий Мамин-Сибиряк");
         $x("//input[@name='phone']").setValue("+79012345678");
         $x("//span[@class='checkbox__box']").click();
-        $x("//button[@class='button button_view_extra button_size_m button_theme_alfa-on-white']").click();
-        $$("[class='input__sub']").get(0).shouldBe(visible);
-        $$("[class='input__sub']").get(0).shouldHave(exactText("Доставка в выбранный город недоступна"));
+        $(".button [class='button__text']").click();
+        $("[data-test-id='city'] .input__sub").shouldBe(visible)
+                .shouldHave(exactText("Доставка в выбранный город недоступна"));
     }
 
     @Test
@@ -131,9 +133,8 @@ public class CardDeliveryTest {
         $x("//input[@name='name']").setValue("Мамин Дмитрий-Сибиряк");
         $x("//input[@name='phone']").setValue("+75678901234");
         $x("//span[@class='checkbox__box']").click();
-        $x("//button[@class='button button_view_extra button_size_m button_theme_alfa-on-white']").click();
-        $$("[class='input__sub']").get(1).shouldBe(visible);
-        $$("[class='input__sub']").get(1).shouldHave(exactText("Неверно введена дата"));
+        $(".button [class='button__text']").click();
+        $("[data-test-id='date'] .input__sub").shouldBe(visible).shouldHave(exactText("Неверно введена дата"));
     }
 
     @Test
@@ -145,9 +146,8 @@ public class CardDeliveryTest {
         $x("//input[@name='name']").setValue("qwerty");
         $x("//input[@name='phone']").setValue("+75612347890");
         $x("//span[@class='checkbox__box']").click();
-        $x("//button[@class='button button_view_extra button_size_m button_theme_alfa-on-white']").click();
-        $$("[class='input__sub']").get(2).shouldBe(visible);
-        $$("[class='input__sub']").get(2).shouldHave(
+        $(".button [class='button__text']").click();
+        $("[data-test-id='name'] .input__sub").shouldBe(visible).shouldHave(
                 exactText("Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы."));
     }
 
@@ -160,52 +160,50 @@ public class CardDeliveryTest {
         $x("//input[@name='name']").setValue("Сибиряк Дмитрий-Мамин");
         $x("//input[@name='phone']").setValue("99999999999999");
         $x("//span[@class='checkbox__box']").click();
-        $x("//button[@class='button button_view_extra button_size_m button_theme_alfa-on-white']").click();
-        $$("[class='input__sub']").get(3).shouldBe(visible);
-        $$("[class='input__sub']").get(3).shouldHave(
-                exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
+        $(".button [class='button__text']").click();
+        $("[data-test-id='phone'] .input__sub").shouldBe(visible)
+                .shouldHave(exactText("Телефон указан неверно. Должно быть 11 цифр, например, +79012345678."));
     }
-
-    //лепим костыли хД
-    private int rng = (int) Math.random() * 15;
-    LocalDate currentDate = LocalDate.now();
-    LocalDate datePlusWeek = LocalDate.now().plusWeeks(1);
-    private String day = String.valueOf(datePlusWeek.getDayOfMonth());
 
     @Test
     void shouldPassValidationWithPopupCitiesListAndCalendarWidget() {
+        int i = 7;
+        String day = String.valueOf(LocalDate.now().plusDays(i).getDayOfMonth());
         $x("//input[@placeholder='Город']").setValue("Кр");
-        $$(".menu-item__control").get(rng).click();
+        $$(".menu-item__control").get(random.nextInt(15)).click();
         $x("//*[@id='root']/div/form/fieldset/div[2]/span/span/span/span/span[1]/span/button").click();
-        if (datePlusWeek.getMonthValue() > currentDate.getMonthValue()) {
+        if (LocalDate.now().plusDays(i).getMonthValue() > LocalDate.now().plusDays(3).getMonthValue()) {
             $x("//*[@class='calendar__arrow calendar__arrow_direction_right']").click();
         }
         $$("td.calendar__day").find(exactText(day)).click();
         $x("//input[@name='name']").setValue("Дмитрий Мамин-Сибиряк");
         $x("//input[@name='phone']").setValue("+79012345678");
         $x("//span[@class='checkbox__box']").click();
-        $x("//button[@class='button button_view_extra button_size_m button_theme_alfa-on-white']").click();
+        $(".button [class='button__text']").click();
         $x("//*[@data-test-id='notification']").shouldBe(visible, Duration.ofSeconds(15));
         $x("//*[@data-test-id='notification']").should(Condition.matchText("Успешно!"));
+        $("[data-test-id='notification'] .notification__content").shouldHave(Condition
+                .text("Встреча успешно забронирована на " + generateDate(i)));
     }
-
-    LocalDate datePlusMonth = LocalDate.now().plusMonths(1);
-    private String day2 = String.valueOf(datePlusMonth.getDayOfMonth());
 
     @Test
     void shouldPassValidationWithPopupCitiesListAndCalendarWidget2() {
+        int i = 30;
+        String day = String.valueOf(LocalDate.now().plusDays(i).getDayOfMonth());
         $x("//input[@placeholder='Город']").setValue("Кр");
-        $$(".menu-item__control").get(rng).click();
+        $$(".menu-item__control").get(random.nextInt(15)).click();
         $x("//*[@id='root']/div/form/fieldset/div[2]/span/span/span/span/span[1]/span/button").click();
-        if (datePlusMonth.getMonthValue() > currentDate.getMonthValue()) {
+        if (LocalDate.now().plusDays(i).getMonthValue() > LocalDate.now().plusDays(3).getMonthValue()) {
             $x("//*[@class='calendar__arrow calendar__arrow_direction_right']").click();
         }
-        $$("td.calendar__day").find(exactText(day2)).click();
+        $$("td.calendar__day").find(exactText(day)).click();
         $x("//input[@name='name']").setValue("Дмитрий Мамин-Сибиряк");
         $x("//input[@name='phone']").setValue("+79012345678");
         $x("//span[@class='checkbox__box']").click();
-        $x("//button[@class='button button_view_extra button_size_m button_theme_alfa-on-white']").click();
+        $(".button [class='button__text']").click();
         $x("//*[@data-test-id='notification']").shouldBe(visible, Duration.ofSeconds(15));
         $x("//*[@data-test-id='notification']").should(Condition.matchText("Успешно!"));
+        $("[data-test-id='notification'] .notification__content").shouldHave(Condition
+                .text("Встреча успешно забронирована на " + generateDate(i)));
     }
 }
